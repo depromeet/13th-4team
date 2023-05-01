8e5f2e8040d6f2caad42184e00b502caf16916c8
Seongtae <stae1102@gmail.com>
2023-05-01 20:46:59 +0900

    feat: 슬랙 알림 서비스 로직 적용

--- apps/server/src/modules/slack/slack.service.ts
+++ apps/server/src/modules/slack/slack.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { ExceptionContents } from 'src/apps/server/exceptions/exception.type';
+import { EnvEnum } from '../env/env.enum';
+import { EnvService } from '../env/env.service';
+import { WebClient } from '@slack/web-api';
+import { SlackBlockType } from './slack.enum';
 
 @Injectable()
-export class SlackService {}
+export class SlackService {
+  constructor(private readonly envService: EnvService) {}
+
+  sendExceptionMessage(exception: ExceptionContents): void {
+    const { statusCode, title, message } = exception;
+    const token: string = this.envService.get<string>(EnvEnum.SLACK_TOKEN);
+    const web: WebClient = new WebClient(token);
+
+    web.chat.postMessage({
+      blocks: [
+        {
+          type: SlackBlockType.SECTION,
+          text: {
+            type: SlackBlockType.MRKDWN,
+            text: ':warning: *An error has occured!* :warning:',
+          },
+        },
+        { type: SlackBlockType.DIVIDER },
+        {
+          type: SlackBlockType.SECTION,
+          text: {
+            type: SlackBlockType.MRKDWN,
+            text: `statusCode: ${statusCode}\ntitle: ${title}\nmessage: ${message}`,
+          },
+        },
+      ],
+      channel: '4팀-알림',
+      text: 'InsiteOut exception message',
+    });
+  }
+}
